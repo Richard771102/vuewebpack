1105e1816dd7f25c1c4f43f3452f858b88651c91
richardhuang <shun.yu1988@gmail.com>
2018-11-30 20:48:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+$color: black;
+
+
+body {
+  background-color: $color;
+}
+</style>
